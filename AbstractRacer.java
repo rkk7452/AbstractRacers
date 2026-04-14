8fd177f6e7d7489be1c60d1bd20a59e8018fe02f
Ryan Kim <ryankim28@students.polytechnic.org>
2026-04-14 10:43:54 -0700

    fixed syntax error in provided code for name of class in AbstractRacer

--- AbstractRacer.java
+++ AbstractRacer.java
@@ -9,43 +9,44 @@
 
 public abstract class AbstractRacer
 {
- // instance variables
- private String name;
- private int position;
- /**
- * Constructs an object according to the subclass
- * that inherits from Racer. (You can't construct an
- * actual Racer object because it's abstract. Go ahead,
- * try it!)
- */
- public Racer(String name)
- {
-    this.name = name;
-    position = 0;
- }
- /**
- * The abstract method move() must be defined by
- * subclasses.
- */
- public abstract void move();
- /**
- * Overrides the toString method
- */
- public String toString()
- {
- // to be completed
- }
- /**
- * The getPosition() method returns the current position
- * of the racer.
- */
+    // instance variables
+    private String name;
+    private int position;
+    /**
+     * Constructs an object according to the subclass
+     * that inherits from Racer. (You can't construct an
+     * actual Racer object because it's abstract. Go ahead,
+     * try it!)
+     */
+    public AbstractRacer(String name)
+    {
+        this.name = name;
+        position = 0;
+    }
+    /**
+     * The abstract method move() must be defined by
+     * subclasses.
+     */
+    public abstract void move();
+    /**
+     * Overrides the toString method
+     */
+    public String toString()
+    {
+    // to be completed
+        return "";
+    }
+    /**
+     * The getPosition() method returns the current position
+     * of the racer.
+     */
 
- // to be completed
- /**
- * The setPosition() method takes a parameter specifying the
- * next position and mutates position to that specified location.
- */
+    // to be completed
+    /**
+     * The setPosition() method takes a parameter specifying the
+     * next position and mutates position to that specified location.
+     */
 
- // to be completed
+    // to be completed
 
 }
